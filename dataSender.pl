9c764dbf23d3694a4267661f16c8c718d645a3be
Robert J. Krasowski <robert@krasowski.net>
2018-07-23 22:47:50 +0000

    Correction for NaN was made. Changing NaN to 0

--- dataSender.pl
+++ dataSender.pl
@@ -15,23 +15,32 @@ use Time::HiRes  qw(tv_interval gettimeofday);
 
 my $server = "192.168.1.51:8086";
 my $data;
-my $output;
+my $output= "";
 my $text = "";
 
-
-open (FH, "beeData.dat") or die "Can not open file $!\n";
+open (FH, "/home/pi/BeeCounter/beeData.dat") or die "Can not open file $!\n";
 
 while (my $line = <FH>)
 	{
 		$text .= $line;
 	}
 
+my $CPUTemp = `cat /sys/class/thermal/thermal_zone0/temp`;
+$CPUTemp = $CPUTemp * 0.001;
+$CPUTemp = sprintf("%.1f",$CPUTemp);
+$CPUTemp = ",CPUTemp=$CPUTemp";
+
+
+
 my @dataArray = split (/\n/,$text);
+
+
 my $dataArray;
 
 foreach (@dataArray)
 	{
-		$output = `curl -i -k -XPOST "https://$server/write?db=beeTraffic&precision=s" --data-binary '$_'`;
+		$_ =~ s/nan/0/;
+		$output = `curl -i -k -XPOST "https://$server/write?db=beeTraffic&precision=s" --data-binary '$_$CPUTemp'`;
 		sleep(1);
 	}
 
@@ -44,8 +53,8 @@ if ($output =~/error/)
 
         }
 
-open (FH, "beeData.dat") or die "Can not read file [$!}\n";               # Open file
-truncate "beeData.dat",0;                # Empty the file
+#open (FH, "/home/pi/BeeCounter/beeData.dat") or die "Can not read file [$!}\n";               # Open file
+truncate "/home/pi/BeeCounter/beeData.dat",0;                # Empty the file
 close FH;
 
 
